0159852df6e0a576b0ad697359c4e36fde7aabbc
Даниил Тимофеев <danite5@mail.ru>
2023-08-23 14:32:32 +0300

    pass test

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,13 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
+
+from django.conf import settings
 
 User = get_user_model()
 
 
-class BaseModel(models.Model):
+class PublishedModel(models.Model):
     created_at = models.DateTimeField(
-        verbose_name='Добавлено',
-        auto_now_add=True)
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
@@ -18,17 +22,17 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Category(BaseModel):
-    title = models.CharField(
-        max_length=256,
-        verbose_name='Заголовок'
-    )
+class Category(PublishedModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
+        max_length=64,
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы '
-                  'латиницы, цифры, дефис и подчёркивание.'
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        )
     )
 
     class Meta:
@@ -36,55 +40,80 @@ class Category(BaseModel):
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.title
+        return self.title[:settings.OSS_INT]
 
 
-class Location(BaseModel):
-    name = models.CharField(
-        max_length=256,
-        verbose_name='Название места'
-    )
+class Location(PublishedModel):
+    name = models.CharField(max_length=256, verbose_name='Название места')
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
+    def __str__(self):
+        return self.name[:settings.OSS_INT]
 
-class Post(BaseModel):
-    title = models.CharField(
-        max_length=256,
-        verbose_name='Заголовок'
-    )
+
+class Post(PublishedModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
+        default=timezone.now,
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — '
-                  'можно делать отложенные публикации.'
+        help_text=(
+            'Если установить дату и время в будущем — можно '
+            'делать отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор публикации'
+        verbose_name='Автор публикации',
     )
     location = models.ForeignKey(
         Location,
+        on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        on_delete=models.SET_NULL,
-        related_name='posts',
-        verbose_name='Местоположение'
+        verbose_name='Местоположение',
     )
     category = models.ForeignKey(
         Category,
-        null=True,
         on_delete=models.SET_NULL,
-        related_name='posts',
-        verbose_name='Категория'
+        null=True,
+        verbose_name='Категория',
     )
+    image = models.ImageField('Фото', upload_to='post_images', blank=True)
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date', )
+        default_related_name = 'posts'
 
     def __str__(self):
-        return self.title
+        return self.title[:settings.OSS_INT]
+
+
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    post = models.ForeignKey(
+        Post,
+        verbose_name='Заголовок поста',
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return self.text[:settings.OSS_INT]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,49 +1,209 @@
-from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
-from blog.models import Category, Post
+from blog.forms import CommentForm, PostForm, ProfileForm
+from blog.models import Category, Comment, Post
 
 
-def index(request):
-    now = timezone.now()
-    template = 'blog/index.html'
-    posts = Post.objects.filter(
-        category__is_published=True,
-        is_published=True,
-        pub_date__lt=now).order_by('-id')[:settings.POSTS_LIMIT]
-    context = {'post_list': posts}
+class ProfileLoginView(LoginView):
+    def get_success_url(self):
+        url = reverse(
+            'blog:profile',
+            args=(self.request.user.get_username(),)
+        )
+        return url
 
+
+def edit_profile(request, name):
+    '''Изменение профиля пользователя.'''
+    user = get_object_or_404(User, username=name)
+    if user.username != request.user.username:
+        return redirect('login')
+    form = ProfileForm(request.POST or None, instance=user)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+    return render(request, 'blog/user.html', context)
+
+
+def get_paginated_posts(posts, page_number):
+    paginator = Paginator(posts, settings.POSTS_PAGE)
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+def info_profile(request, name):
+    '''Информация о профиле пользователя.'''
+    template = 'blog/profile.html'
+    user = get_object_or_404(User, username=name)
+    profile_posts = user.posts.all()
+    page_number = request.GET.get('page')
+    page_obj = get_paginated_posts(profile_posts, page_number)
+    context = {
+        'profile': user,
+        'page_obj': page_obj,
+    }
     return render(request, template, context)
 
 
-def post_detail(request, post_id):
-    now = timezone.now()
-    posts = get_object_or_404(
-        Post,
-        id=post_id,
-        is_published=True,
-        pub_date__lt=now,
-        category__is_published=True
-    )
-    template = 'blog/detail.html'
-    context = {'post': posts}
+class PostListView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = '-pub_date'
+    paginate_by = settings.POSTS_PAGE
 
-    return render(request, template, context)
+    def get_queryset(self):
+        return Post.objects.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True,
+        ).select_related('author', 'category', 'location')
 
 
-def category_posts(request, slug):
-    now = timezone.now()
-    template = 'blog/category.html'
+def category_posts(request, category_slug):
+    '''Отображение по категории постов'''
+    templates = 'blog/category.html'
+    current_time = timezone.now()
     category = get_object_or_404(
         Category,
-        slug=slug,
         is_published=True,
+        slug=category_slug
     )
-    posts = category.posts.all().filter(pub_date__lt=now, is_published=True)
+    post_list = category.posts.filter(
+        pub_date__lte=current_time,
+        is_published=True,
+    ).select_related('category')
+    paginator = Paginator(post_list, settings.POSTS_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    posts = page_obj.object_list
     context = {
         'category': category,
-        'post_list': posts
+        'posts': posts,
+        'page_obj': page_obj
+    }
+    return render(request, templates, context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create.html'
+
+    def form_valid(self, form):
+        '''Проверка валидности формы.'''
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        '''Получение адреса.'''
+        url = reverse(
+            'blog:profile',
+            args=(self.request.user.get_username(),)
+        )
+        return url
+
+
+class DispatchMixin:
+    def dispatch(self, request, *args, **kwargs):
+        '''Отправляет изменения/удаления поста'''
+        self.post_id = kwargs['pk']
+        if self.get_object().author != request.user:
+            return redirect('blog:post_detail', pk=self.post_id)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostUpdateView(LoginRequiredMixin, DispatchMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create.html'
+
+    def get_success_url(self):
+        '''Получение адреса.'''
+        url = reverse('blog:post_detail', args=(self.post_id,))
+        return url
+
+
+class PostDeleteView(LoginRequiredMixin, DispatchMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+    template_name = 'blog/create.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+
+    def get_object(self):
+        queryset = Post.objects.filter(
+            Q(is_published=True) | Q(author=self.request.user)
+        )
+        return get_object_or_404(
+            queryset,
+            pk=self.kwargs.get('pk'),
+        )
+
+    def get_context_data(self, **kwargs):
+        '''Получение данных контекста'''
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        context['comments'] = (
+            self.object.comments.select_related(
+                'author'
+            )
+        )
+        return context
+
+
+@login_required
+def add_comment(request, pk):
+    '''Добавление комментария'''
+    post = get_object_or_404(Post, pk=pk)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('blog:post_detail', pk=pk)
+
+
+@login_required
+def edit_comment(request, comment_id, post_id):
+    '''Изменение комментария'''
+    instance = get_object_or_404(Comment, id=comment_id, post_id=post_id)
+    form = CommentForm(request.POST or None, instance=instance)
+    if instance.author != request.user:
+        return redirect('blog:post_detail', pk=post_id)
+    context = {
+        'form': form,
+        'comment': instance
     }
 
-    return render(request, template, context)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:post_detail', pk=post_id)
+    return render(request, 'blog/comment.html', context)
+
+
+@login_required
+def delete_comment(request, comment_id, post_id):
+    '''Удаление комментария'''
+    instance = get_object_or_404(Comment, id=comment_id, post_id=post_id)
+    if instance.author != request.user:
+        return redirect('blog:post_detail', pk=post_id)
+    context = {'comment': instance}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('blog:post_detail', pk=post_id)
+    return render(request, 'blog/comment.html', context)
